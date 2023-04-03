1dd02ae4e099b3d3cdc03dfa3b8cab0519739ad2
fbizza <francesco.bizzarri@stud.unifi.it>
2023-04-03 13:27:37 +0200

    Add elapsed time measurement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.neural_network import MLPRegressor
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+import time
 
 np.random.seed(29)
 
@@ -16,13 +17,27 @@ y_sample = np.sin(x_sample) * (1 + x_sample / 2)
 
 # Fit multilayer perceptron and kernel regressor
 mlp = MLPRegressor(hidden_layer_sizes=(50, 100, 200, 100, 50), activation='relu', solver='adam', max_iter=300)
+t1 = time.perf_counter()
 mlp.fit(x_sample, y_sample)
+t2 = time.perf_counter()
+mlp_fit_elapsed_time = t2 - t1
+
+t1 = time.perf_counter()
 kern_reg = sm.nonparametric.KernelReg(endog=y_sample, exog=x_sample, var_type='c')
+t2 = time.perf_counter()
+kern_reg_fit_elapsed_time = t2 - t1
 
 
 # Predict y values for x data
+t1 = time.perf_counter()
 y_test = mlp.predict(x_test)
+t2 = time.perf_counter()
+mlp_predict_elapsed_time = t2 - t1
+
+t1 = time.perf_counter()
 y_test_kreg, _ = kern_reg.fit(x_test)
+t2 = time.perf_counter()
+kern_reg_predict_elapsed_time = t2 - t1
 
 # Plot the results
 plt.scatter(x_sample, y_sample, color='red', s=6, label='Samples')
@@ -31,3 +46,6 @@ plt.plot(x_test, y_test, color='orange', label='MLP fit')
 plt.plot(x_test, y_test_kreg, color='green', label='Kernel Regression fit')
 plt.legend()
 plt.show()
+
+print(f"MLP took {mlp_fit_elapsed_time:.3f} seconds to fit and {mlp_predict_elapsed_time:.3f} to predict")
+print(f"Kernel regression took {kern_reg_fit_elapsed_time:.3f} seconds to fit and {kern_reg_predict_elapsed_time:.3f} to predict")
